dc6daf69b772c17ccc419706fd9f8c4c05445753
Hanna Angeria <haan2402@student.miun.se>
2025-01-25 12:34:52 +0100

    lagt till for hamburger meny i html, och stylat det med css

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-console.log("Hej från app.js")
+"use strict";
+
+//element för öppna, stänga menyn i mobilläge
+let openButton = document.getElementById("open-menu");
+let closeButton = document.getElementById("close-menu");
+
+//eventlyssnare för att öppna menyn och stänga menyn i mobilläge
+openButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
+
+//funktion för att toggla mellan öppna och stänga menyn i mobilläge
+function toggleMenu() {
+    let menuEl = document.getElementById("nav-menu");
+
+    //hämtar in styling från css
+    let style = window.getComputedStyle(menuEl);
+
+    if(style.display === "none") {
+        menuEl.style.display ="block";
+    } else {
+        menuEl.style.display = "none";
+    }
+}
